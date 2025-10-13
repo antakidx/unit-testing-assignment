ac45795083d13120d675d8b6747776d8730dc97f
Dominic Antaki <131810738+antakidx@users.noreply.github.com>
2025-10-12 23:00:07 -0400

    Update test.py

--- sections/section2/test.py
+++ sections/section2/test.py
@@ -5,19 +5,19 @@ class TestCalc(unittest.TestCase):
 
     def test_add(self):
         calc = Calculations(8, 2)
-        self.assertEqual(calc.get_sum(), 10, 'Addition failed.')
+        self.assertEqual(calc.add(), 10, 'Addition failed.')
 
     def test_subtract(self):
         calc = Calculations(8, 2)
-        self.assertEqual(calc.get_difference(), 6, 'Subtraction failed.')
+        self.assertEqual(calc.subtract(), 6, 'Subtraction failed.')
 
     def test_multiply(self):
         calc = Calculations(8, 2)
-        self.assertEqual(calc.get_product(), 16, 'Multiplication failed.')
+        self.assertEqual(calc.multiply(), 16, 'Multiplication failed.')
 
     def test_divide(self):
         calc = Calculations(8, 2)
-        self.assertEqual(calc.get_quotient(), 4, 'Division failed.')
+        self.assertEqual(calc.divide(), 4, 'Division failed.')
 
 if __name__ == '__main__':
     unittest.main()
