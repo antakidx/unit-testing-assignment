133ab2956bac1c056a1e31c70c729bf1c284ec6a
Dominic Antaki <131810738+antakidx@users.noreply.github.com>
2025-10-12 22:52:16 -0400

    Update test.py

--- sections/section1/test.py
+++ sections/section1/test.py
@@ -4,9 +4,9 @@ from my_code.my_calculations import Calculations
 
 class TestCalculations(unittest.TestCase):
 
-    def test_sum(self):
-        calculation = Calculations(8, 2)
-        self.assertEqual(calculation.get_sum(), 11, 'The sum is wrong.')
+    def test_addition(self):
+        calc = Calculations(8, 2)
+        self.assertEqual(calc.get_sum(), 11, 'Incorrect result for addition.')
 
 
 if __name__ == '__main__':
